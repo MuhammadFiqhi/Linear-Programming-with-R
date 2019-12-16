72d837626f46b14261e230a828e0cf3678dc68ef
MuhammadFiqhi <58929448+MuhammadFiqhi@users.noreply.github.com>
2019-12-16 14:01:39 +0700

    Create Nomor 2.R

---
+++ Nomor 2.R
@@ -0,0 +1,26 @@
+# Import lpSolve package
+library(lpSolve)
+
+# Set coefficients of the objective function
+f.obj <- c(8000,12000)
+
+# Set matrix corresponding to coefficients of constraints by rows
+# Do not consider the non-negative constraint; it is automatically assumed
+f.con <- matrix(c(3,4,
+                  1,3
+                  ),nrow=2,byrow=TRUE)
+
+# Set unequality signs
+f.dir <- c("<=",
+           "<=")
+
+# Set right hand side coefficients
+f.rhs <- c(60,
+           30)
+
+# Final value (z)
+# Max Value
+lp("max",f.obj,f.con,f.dir,f.rhs)
+
+# Variables final values
+lp("max",f.obj,f.con,f.dir,f.rhs)$solution
